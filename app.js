7a7efb460a6d31dbcc5a72f1cfc55e17a1741914
alititi-jackie <fengjiancheng8@gmail.com>
2026-04-19 12:47:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 let data = [];
+let current = [];
 
-fetch("data.json")
-  .then(res => res.json())
+fetch("./data/numbers.json")
+  .then(r => r.json())
   .then(res => {
     data = res;
-    render(data);
+    current = res;
+    render(res);
   });
 
-function render(list) {
+function render(list){
   const box = document.getElementById("list");
   box.innerHTML = "";
 
   list.forEach(item => {
+
     const div = document.createElement("div");
     div.className = "card";
 
     if(item.sold) div.classList.add("sold");
+    if(item.hot) div.classList.add("hot");
 
     div.innerHTML = `
       <h3>${format(item.number)}</h3>
-      <p>${item.sold ? "已售出" : (item.price ? "$"+item.price : "咨询")}</p>
+      <div class="price">
+        ${item.sold ? "已售出" : (item.price ? "$"+item.price : "咨询")}
+      </div>
     `;
 
     div.onclick = () => {
-      if(item.sold) return alert("该号码已售出");
+      if(item.sold){
+        alert("该号码已售出");
+        return;
+      }
       openModal(item.number);
     };
 
@@ -31,19 +40,28 @@ function render(list) {
   });
 }
 
+/* 格式化号码 */
 function format(num){
   return num.replace(/(\d{3})(\d{3})(\d{4})/, "$1 $2 $3");
 }
 
 /* 搜索 */
 document.getElementById("searchInput").oninput = function(){
-  const val = this.value;
-  render(data.filter(d => d.number.includes(val)));
+  const v = this.value;
+  const res = data.filter(d => d.number.includes(v));
+  render(res);
 };
 
 /* 分类 */
-function filterType(type){
-  if(type === "all") return render(data);
+function filter(type){
+
+  if(type === "all"){
+    return render(data);
+  }
+
+  if(type === "hot"){
+    return render(data.filter(d => d.hot));
+  }
 
   if(type === "triple"){
     return render(data.filter(d => /(\\d)\\1\\1/.test(d.number)));
@@ -52,15 +70,11 @@ function filterType(type){
   if(type === "quad"){
     return render(data.filter(d => /(\\d)\\1\\1\\1/.test(d.number)));
   }
-
-  if(type === "straight"){
-    return render(data.filter(d => "1234567890".includes(d.number)));
-  }
 }
 
 /* 弹窗 */
 function openModal(num){
-  document.getElementById("modalNumber").innerText = num;
+  document.getElementById("m-number").innerText = num;
   document.getElementById("modal").style.display = "block";
 }
 
